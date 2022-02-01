56cc0d81b1e64a7b3dbe41637f8aca50df148984
Mofizur Rahman <mofizur853@gmail.com>
2022-02-02 01:04:30 +0600

    Inisial commit

---
+++ app/src/main/java/com/ideasoft/saveimagesqlite/User.java
@@ -0,0 +1,28 @@
+package com.ideasoft.saveimagesqlite;
+
+public class User {
+
+    private int id;
+    private byte[] pp;
+
+    public User(int id, byte[] pp) {
+        this.id = id;
+        this.pp = pp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public byte[] getPp() {
+        return pp;
+    }
+
+    public void setPp(byte[] pp) {
+        this.pp = pp;
+    }
+}
